3e675fca196d3a5f2becbc706b4d58e860ba9125
Pekka Nurmi <peknur@gmail.com>
2020-12-13 15:33:24 +0200

    add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 name = "dockercontext"
 
 setup(
     name=name,
     version="0.1",
     description="Docker container context manager",
+    long_description=long_description,
     author="Pekka Nurmi",
     author_email="peknur@gmail.com",
     packages=[name],
